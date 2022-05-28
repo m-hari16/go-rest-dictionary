19adadc5327c610fda20de907312bdb570d50d08
m-hari16 <active.hari16@gmail.com>
2022-05-28 22:08:21 +0700

    install package

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/api", func(c *fiber.Ctx) error {
+		return c.JSON(&fiber.Map{"message": "Welcome to API"})
+	})
+
+	app.Listen(":8000")
+}
