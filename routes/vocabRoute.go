72a68361824f8f40cdd3539d57bc0b3e48389938
m-hari16 <active.hari16@gmail.com>
2022-06-01 17:50:56 +0700

    set up routes for it's endpoint

---
+++ routes/vocabRoute.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"go-rest-dictionary/controller"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func VocabRoutes(app *fiber.App) {
+	api := app.Group("/api")
+	api.Post("/vocab", controller.StoreVocab)
+	api.Get("/vocab", controller.IndexVocab)
+	api.Get("/vocab/:word", controller.Translate)
+}
